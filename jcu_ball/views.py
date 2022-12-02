9f78b41ab5cae937183861938571f025e2828b24
jake.richman <jake.2323@live.com>
2022-12-02 06:55:17 -0800

    .

--- jcu_ball/views.py
+++ jcu_ball/views.py
@@ -24,7 +24,7 @@ class BallReservationListView(generic.ListView):
             return BallReservation.objects.all()
 
     def get_context_data(self, **kwargs):
-        context = super(BallReservationListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['filter'] = self.request.GET.get('filter')
         return context
 
